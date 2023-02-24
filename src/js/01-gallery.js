44e821e141b8e2902d9bf39be7949218ffaa6f74
Ruslan-Ts <r.ts@me.com>
2023-02-24 18:42:02 +0100

    er

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+return `<a class="gallery__item" href="${original}">
+<img class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"/>
+</a>`}).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+
+// function onGalleryClick(evt) {
+//     evt.preventDefault();
+//     const lightbox = 
+new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
+const STORAGE_KEY = "feedback-form-state";
 
+const form = document.querySelector('.feedback-form');
+const text = document.querySelector('textarea');
+const input = document.querySelector('input');
+
+let formData = {};
+
+populateInputForm();
+
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', throttle(onInputForm, 500));
+
+function onSubmitForm(evt) {
+    evt.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
+}
+function onInputForm(evt) {
+    formData(e.target.name) = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateInputForm() {
+    const parsedData = localStorage.getItem(STORAGE_KEY);
+    if (parsedData) {
+        const formKeys = JSON.parse(parsedData);
+        if (formKeys.email !== undefined) input.value = formKeys.email;
+        if (formKeys.message !== undefined) text.value = formKeys.message;
+    }
+}
